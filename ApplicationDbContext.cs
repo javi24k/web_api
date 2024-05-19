a1e560b74eb256c4ee24759c9a5fd6983fa2e381
javi24k <142742451+javi24k@users.noreply.github.com>
2024-05-18 21:30:58 -0400

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace web_api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,13 @@
+namespace web_api
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int precio { get; set; }
+        public int stock { get; set; }
+        public string categoria { get; set; }
+
+    }
+}
